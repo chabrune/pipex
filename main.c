9d9d6b60797edc1311e237e3098c07e90df6c98e
Acid-Division <96349900+Charli420@users.noreply.github.com>
2023-02-25 21:15:22 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -6,66 +6,62 @@
 /*   By: chabrune <charlesbrunet51220@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 16:08:19 by chabrune          #+#    #+#             */
-/*   Updated: 2023/02/22 17:36:23 by chabrune         ###   ########.fr       */
+/*   Updated: 2023/02/25 21:13:38 by chabrune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	pipex(int fd1, int fd2, char **argv, char **envp)
+void	pipex(t_allyouneed *ola)
 {
-	int pid;
-	int fd[2];
-	pipe(fd);
-	pid = fork();
-	if(pid == - 1)
-		return(1);
-	else if(pid == 0)
-		child_process(fd1, fd, envp, argv);
+	pipe(ola->fdraw);
+	ola->pid = fork();
+	if(ola->pid == - 1)
+		return;
+	else if(ola->pid == 0)
+		child_process(ola);
 	else
-		parent_process(fd2,fd, envp, argv);
+		parent_process(ola);
 }
 
-int	child_process(int fd1, int *fd, char **envp, char **argv)
+int	child_process(t_allyouneed *ola)
 {
-	if(dup2(fd1, STDIN_FILENO) == -1)
+	if(dup2(ola->fdin, STDIN_FILENO) == -1)
 		return(2);
-	if(dup2(fd[1], STDOUT_FILENO) == -1)
+	if(dup2(ola->fdraw[1], STDOUT_FILENO) == -1)
 		return(3);
-	close(fd[1]);
-	close(fd1);
-	hello(envp, argv, 2);
+	close(ola->fdin);
+	close(ola->fdraw[0]);
 	return(0);
 }
 
-int	parent_process(int fd2, int *fd, char **envp, char **argv)
+int	parent_process(t_allyouneed *ola)
 {
-	int status;
-	waitpid(-1, &status, 0);
-	if(dup2(fd2, STDOUT_FILENO) == -1)
+	wait(NULL);
+	if(dup2(ola->fdout, STDOUT_FILENO) == -1)
 		return(4);
-	if(dup2(fd[0], STDIN_FILENO) == -1)
+	if(dup2(ola->fdraw[0], STDIN_FILENO) == -1)
 		return(5);
-	close(fd[0]);
-	close(fd2);
-	hello(envp, argv, 3);
+	close(ola->fdraw[1]);
+	close(ola->fdout);
+	hello(ola);
 	return(0);
 }
 
-char	*find_path(char **envp)
+char	*find_path(t_allyouneed *ola)
 {
 	int i;
 
 	i = -1;
-	while(envp[++i])
+	while(ola->env[++i])
 	{
-		if(strncmp(envp[i], "PATH", 4) == 0)
-			return(envp[i]);
+		if(strncmp(ola->env[i], "PATH", 5 ) == 0)
+			return(ola->env[i]);
 	}
 	return(0);
 }
 
-int	hello(char **envp, char **argv, int ar)
+int	hello(t_allyouneed *ola)
 {
 	char *pathenv;
 	char **mypath;
@@ -73,28 +69,36 @@ int	hello(char **envp, char **argv, int ar)
 	int i;
 
 	i = -1;
-	cmdargs = ft_split(argv[ar], ' ');
-	pathenv = find_path(envp);
+	cmdargs = ft_split(ola->ar[2], ' ');
+	pathenv = find_path(ola);
 	mypath = ft_split(pathenv, ':');
 	while(mypath[++i])
 	{
-		char *cmd = ft_strjoin(mypath[i], argv[2]);
-		execve(cmd, cmdargs, envp);
+		char *cmd = ft_strjoin(mypath[i], ola->ar[2]);
+		execve(cmd, cmdargs, ola->env);
 	    free(cmd);
 	}
 	return (EXIT_FAILURE);
 }
 
+int	get_all_you_need(char **ar, char **env, t_allyouneed *ola)
+{
+	ar = ola->ar;
+	env = ola->env;
+	ola->fdin = open(ar[1], O_RDONLY);
+	ola->fdout = open(ar[4], O_CREAT | O_RDWR | O_TRUNC, 0644); // Create a file if it doesnt exit -- TRUNC == reset file a 0
+	if(ola->fdin < 0 || ola->fdout < 0)
+		return(1);
+	return(0);
+}
+
 int main(int argc, char **argv, char **envp)
 {
 	(void)argc;
-	int fd1;
-	int fd2;
-
-	fd1 = open(argv[1], O_RDONLY);
-	fd2 = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 0644); // Create a file if it doesnt exit -- TRUNC == reset file a 0
-	if(fd1 < 0 || fd2 < 0)
-		return(1);
-	pipex(fd1, fd2, argv, envp);
+	t_allyouneed *ola;
+	ola = NULL;
+	ola = malloc(sizeof(ola));
+	get_all_you_need(argv, envp, ola);
+	pipex(ola);
 	return(0);
 }
