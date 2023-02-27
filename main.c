0ea517c7e380501ab506c23b23beea4676b17a9a
Acid-Division <96349900+Charli420@users.noreply.github.com>
2023-02-27 20:40:21 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: chabrune <charlesbrunet51220@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 16:08:19 by chabrune          #+#    #+#             */
-/*   Updated: 2023/02/25 21:13:38 by chabrune         ###   ########.fr       */
+/*   Updated: 2023/02/27 20:37:54 by chabrune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,91 +14,145 @@
 
 void	pipex(t_allyouneed *ola)
 {
-	pipe(ola->fdraw);
-	ola->pid = fork();
-	if(ola->pid == - 1)
-		return;
-	else if(ola->pid == 0)
-		child_process(ola);
+	if(pipe(ola->tube) == -1)
+	{
+		perror("pipe");
+		exit(EXIT_FAILURE);
+	}
+	ola->pid1 = fork();
+	if(ola->pid1 == - 1)
+	{
+		perror("fork");
+        exit(EXIT_FAILURE);
+	}
+	else if(ola->pid1 == 0)
+		child1_process(ola);
 	else
-		parent_process(ola);
+	{
+		ola->pid2 = fork();
+		if(ola->pid2 == -1)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+		else if(ola->pid2 == 0)
+			child2_process(ola);
+		else
+			parent_process(ola);
+	}
 }
 
-int	child_process(t_allyouneed *ola)
+void	parent_process(t_allyouneed *ola)
+{
+	close(ola->fdin);
+	close(ola->fdout);
+	close(ola->tube[0]);
+	close(ola->tube[1]);
+	waitpid(ola->pid1, NULL, 0);
+	waitpid(ola->pid2, NULL, 0);
+}
+int	child1_process(t_allyouneed *ola)
 {
 	if(dup2(ola->fdin, STDIN_FILENO) == -1)
-		return(2);
-	if(dup2(ola->fdraw[1], STDOUT_FILENO) == -1)
-		return(3);
-	close(ola->fdin);
-	close(ola->fdraw[0]);
+	{
+            perror("dup2");
+            exit(EXIT_FAILURE);
+    }
+	if(dup2(ola->tube[1], STDOUT_FILENO) == -1)
+	{
+		perror("dup2");
+		exit(EXIT_FAILURE);
+	}
+	close(ola->tube[0]);
+	ola->cmd_args = ft_split(ola->ar[2], ' ');
+	ola->cmd = get_cmd(ola->cmd_paths, ola->cmd_args[0]);
+	execve(ola->cmd, ola->cmd_args, ola->env);
+	perror("exceve");
+	exit(EXIT_FAILURE);
 	return(0);
 }
 
-int	parent_process(t_allyouneed *ola)
+int	child2_process(t_allyouneed *ola)
 {
-	wait(NULL);
 	if(dup2(ola->fdout, STDOUT_FILENO) == -1)
-		return(4);
-	if(dup2(ola->fdraw[0], STDIN_FILENO) == -1)
-		return(5);
-	close(ola->fdraw[1]);
-	close(ola->fdout);
-	hello(ola);
+	{
+            perror("dup2");
+            exit(EXIT_FAILURE);
+    }
+	if(dup2(ola->tube[0], STDIN_FILENO) == -1)
+	{
+            perror("dup2");
+            exit(EXIT_FAILURE);
+    }
+	close(ola->tube[1]);
+	ola->cmd_args = ft_split(ola->ar[3], ' ');
+	ola->cmd = get_cmd(ola->cmd_paths, ola->cmd_args[0]);
+	execve(ola->cmd, ola->cmd_args, ola->env);
+	perror("execve");
+	exit(EXIT_FAILURE);
 	return(0);
 }
 
-char	*find_path(t_allyouneed *ola)
+char	*find_path(char **env)
 {
 	int i;
 
 	i = -1;
-	while(ola->env[++i])
-	{
-		if(strncmp(ola->env[i], "PATH", 5 ) == 0)
-			return(ola->env[i]);
-	}
-	return(0);
+	while(strncmp("PATH", *env, 4))
+		env++;
+	return(*env + 5);
 }
 
-int	hello(t_allyouneed *ola)
+char *get_cmd(char **paths, char *cmd)
 {
-	char *pathenv;
-	char **mypath;
-	char **cmdargs;
-	int i;
+	char	*tmp;
+	char	*command;
 
-	i = -1;
-	cmdargs = ft_split(ola->ar[2], ' ');
-	pathenv = find_path(ola);
-	mypath = ft_split(pathenv, ':');
-	while(mypath[++i])
+	while (*paths)
 	{
-		char *cmd = ft_strjoin(mypath[i], ola->ar[2]);
-		execve(cmd, cmdargs, ola->env);
-	    free(cmd);
+		tmp = ft_strjoin(*paths, "/");
+		command = ft_strjoin(tmp, cmd);
+		free(tmp);
+		if (access(command, 0) == 0)
+			return (command);
+		free(command);
+		paths++;
 	}
-	return (EXIT_FAILURE);
+	return (NULL);
+}
+
+void	close_fd(t_allyouneed *ola)
+{
+	close(ola->tube[0]);
+	close(ola->tube[1]);
 }
 
 int	get_all_you_need(char **ar, char **env, t_allyouneed *ola)
 {
-	ar = ola->ar;
-	env = ola->env;
+	ola->ar = ar;
+	ola->env = env;
+	ola->paths = find_path(env);
+	ola->cmd_paths = ft_split(ola->paths, ':');
 	ola->fdin = open(ar[1], O_RDONLY);
+	if(ola->fdin == -1)
+	{
+		perror("open");
+        exit(EXIT_FAILURE);
+	}
 	ola->fdout = open(ar[4], O_CREAT | O_RDWR | O_TRUNC, 0644); // Create a file if it doesnt exit -- TRUNC == reset file a 0
-	if(ola->fdin < 0 || ola->fdout < 0)
-		return(1);
+	if(ola->fdout == -1)
+	{
+		perror("open");
+        exit(EXIT_FAILURE);
+	}
 	return(0);
 }
 
 int main(int argc, char **argv, char **envp)
 {
 	(void)argc;
-	t_allyouneed *ola;
-	ola = NULL;
-	ola = malloc(sizeof(ola));
-	get_all_you_need(argv, envp, ola);
-	pipex(ola);
+	t_allyouneed ola;
+	get_all_you_need(argv, envp, &ola);
+	pipex(&ola);
 	return(0);
 }
--- pipex.h
+++ pipex.h
@@ -4,10 +4,31 @@
 # include "/Users/chabrune/Desktop/42-school/ft_printf/ft_printf.h"
 # include "/Users/chabrune/Desktop/42-school/libft/libft.h"
 
+typedef struct s_allyouneed
+{
+	char **env;
+	char **ar;
+	int tube[2];
+	char **cmd_paths;
+	char *paths;
+	char **cmd_args;
+	char *cmd;
+	int pid1;
+	int pid2;
+	int fdin;
+	int fdout;
+}	t_allyouneed;
 
-#include <fcntl.h>
-#include <stdio.h>
-#include <unistd.h>
+int	    child1_process(t_allyouneed *ola);
+char 	*get_cmd(char **cmd_paths, char *cmd);
+void 	pipex(t_allyouneed *ola);
+void	parent_process(t_allyouneed *ola);
+int	    child2_process(t_allyouneed *ola);
+int 	hello(t_allyouneed *ola, char *cmd);
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <unistd.h>
 
 
 #endif
